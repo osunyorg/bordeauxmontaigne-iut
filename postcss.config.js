fb60000bc4105e32bfeab70a7ecc0664ce2d982d
sebousan <sebousan@gmail.com>
2022-01-27 11:18:41 +0100

    menu

--- postcss.config.js
+++ postcss.config.js
@@ -11,6 +11,7 @@ module.exports = {
                 'show',
                 'active',
                 'collapsed',
+                /^dropdown/,
                 /^nav-level-/,
                 /^is-/,
                 /^has-/,
