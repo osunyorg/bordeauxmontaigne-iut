f720eb17972383c98d24c1d53d6c8cb5253006f6
sebousan <sebousan@gmail.com>
2022-05-23 14:46:22 +0200

    remove specific sticky

--- assets/js/main.js
+++
@@ -1,7 +0,0 @@
-import './vendors/bootstrap';
-import './vendors/lightbox';
-import './vendors/carousel';
-import './vendors/cookie-banner';
-import './theme';
-import './site/stickyNav';
-import './site/timeline';
